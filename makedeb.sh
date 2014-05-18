6009e158334e854cb5db2319db645b1b583429e3
Jérémy HERGAULT <jhergault@MacBook-Pro-de-Jeremy.local>
2014-05-18 12:05:26 +0200

    Correction installation

--- makedeb.sh
+++ makedeb.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-cd bin_hexapod
+# Creation des dossiers necessaires
+mkdir bin_hexapod/var/
+mkdir bin_hexapod/var/www/
+
 # Copie des fichiers requis
-cp ../hexapod/exe/hexapod.jar usr/local/bin/hexapod/
-cp ../wwwRemote/gamepadapi.js var/www/
-cp ../wwwRemote/gamepadapi.js var/www/
-cp ../wwwRemote/gamepadapi.js var/www/
+cp hexapod/exe/hexapod.jar bin_hexapod/usr/local/bin/hexapod/
+cp wwwRemote/gamepadapi.js bin_hexapod/var/www/
+cp wwwRemote/index.html bin_hexapod/var/www/
+cp wwwRemote/virtualjoystick.js bin_hexapod/var/www/
 
 # Construction du paquet
-sudo dpkg-deb --build hexapod.deb
+sudo dpkg-deb --build bin_hexapod
+
+# Finalisation
+mv bin_hexapod.deb hexapod.deb
+chmod +x hexapod.deb
