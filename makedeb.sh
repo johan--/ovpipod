252bebf9e98d788ad1d9d823a0faff58a98d13c8
Jérémy HERGAULT <jhergault@MacBook-Pro-de-Jeremy.local>
2014-05-18 11:42:31 +0200

    allegement routine install

--- makedeb.sh
+++ makedeb.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 cd bin_hexapod
+# Copie des fichiers requis
 cp ../hexapod/exe/hexapod.jar usr/local/bin/hexapod/
+cp ../wwwRemote/gamepadapi.js var/www/
+cp ../wwwRemote/gamepadapi.js var/www/
+cp ../wwwRemote/gamepadapi.js var/www/
+
+# Construction du paquet
 sudo dpkg-deb --build hexapod.deb
